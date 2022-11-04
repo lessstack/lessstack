611bb5356d10164c952afcc3b05c5ca00b578026
cahnory <cahnory@gmail.com>
2022-11-04 21:25:16 +0100

    fix: issue when linting from cli

--- examples/client-web/src/components/Button/Button.d.ts
+++ examples/client-web/src/components/Button/Button.d.ts
@@ -1,8 +1,8 @@
 import type { ButtonHTMLAttributes, FC, ReactNode } from "react";
 declare type ButtonProps = {
-    children: ReactNode;
-    isDisabled: boolean;
-    type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  children: ReactNode;
+  isDisabled: boolean;
+  type?: ButtonHTMLAttributes<HTMLButtonElement>["type"];
 };
 declare const Button: FC<ButtonProps>;
 export default Button;
--- examples/client-web/webpack.config.js
+++ examples/client-web/webpack.config.js
@@ -1,1 +1,2 @@
+/* eslint-env node */
 module.exports = require("@lessstack/webpack-config/build").default;
