27bc2050eb69b66d4f7f8610d56505d49ed9c962
cahnory <cahnory@gmail.com>
2022-11-09 08:19:31 +0100

    feat: add vanilla-extract to client-web-example

--- examples/client-web/webpack.config.js
+++ examples/client-web/webpack.config.js
@@ -1,2 +1,11 @@
 /* eslint-env node */
-module.exports = require("@lessstack/webpack-config/build").default;
+const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const { createConfig } = require("@lessstack/webpack-config/build");
+
+module.exports = createConfig({
+  postProcess: (configs) =>
+    configs.map((config) => {
+      config.plugins.push(new VanillaExtractPlugin());
+      return config;
+    }),
+});
